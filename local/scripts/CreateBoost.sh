3cd7d288f00dfb285f4c3ebb71ad1005b3cb4da6
Dirk Eddelbuettel <edd@debian.org>
2015-01-05 07:15:13 -0600

    better indentation of initial library list

--- local/scripts/CreateBoost.sh
+++ local/scripts/CreateBoost.sh
@@ -94,7 +94,9 @@ echo "Unpacking ${boosttargz} into LocalDir (ie ${localdir})."
 
 echo "Copying Boost libraries into BH"
 
-boostlibs="bind concept config container date_time detail exception functional integer interprocess intrusive io iterator math move mpl numeric pending preprocessor random range smart_ptr tupe typeof type_trains unordered utility uuid"
+boostlibs="bind concept config container date_time detail exception functional integer \
+           interprocess intrusive io iterator math move mpl numeric pending preprocessor \
+           random range smart_ptr tupe typeof type_trains unordered utility uuid"
 
 ## this copies the Boost libraries listed in ${boostlibs} from the
 ## Boost sources in ${boostroot} into the target directory ${pkgincl}
