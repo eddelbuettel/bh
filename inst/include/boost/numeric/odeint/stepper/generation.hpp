cac10536b5d3c7ca2514ce826891b1da270771cd
Dirk Eddelbuettel <edd@debian.org>
2015-05-13 06:39:02 -0500

    added Boost 1.58

--- inst/include/boost/numeric/odeint/stepper/generation.hpp
+++ inst/include/boost/numeric/odeint/stepper/generation.hpp
@@ -6,8 +6,8 @@
  Forward header for the factory functions. Includes all files from the generation directory.
  [end_description]
 
- Copyright 2009-2011 Karsten Ahnert
- Copyright 2009-2011 Mario Mulansky
+ Copyright 2011 Karsten Ahnert
+ Copyright 2011 Mario Mulansky
 
  Distributed under the Boost Software License, Version 1.0.
  (See accompanying file LICENSE_1_0.txt or
@@ -24,7 +24,7 @@
 #include <boost/numeric/odeint/stepper/generation/generation_controlled_runge_kutta.hpp>
 #include <boost/numeric/odeint/stepper/generation/generation_dense_output_runge_kutta.hpp>
 
-#include <boost/numeric/odeint/stepper/generation/generation_runge_kutta_cash_karp54_cl.hpp>
+#include <boost/numeric/odeint/stepper/generation/generation_runge_kutta_cash_karp54_classic.hpp>
 #include <boost/numeric/odeint/stepper/generation/generation_runge_kutta_cash_karp54.hpp>
 #include <boost/numeric/odeint/stepper/generation/generation_runge_kutta_dopri5.hpp>
 #include <boost/numeric/odeint/stepper/generation/generation_runge_kutta_fehlberg78.hpp>
