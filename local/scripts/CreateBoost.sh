3799bebe515fba2376b319d4d7b357a324578ca7
Dirk Eddelbuettel <edd@debian.org>
2019-12-15 09:47:00 -0600

    updated helper script for Boost 1.72

--- local/scripts/CreateBoost.sh
+++ local/scripts/CreateBoost.sh
@@ -2,7 +2,7 @@
 ##
 ## CreateBoost.sh -- derived from CreateBoost.R
 ##
-## Jay Emerson and Dirk Eddelbuettel,  2012 - 2016
+## Jay Emerson and Dirk Eddelbuettel,  2012 - 2019
 
 
 ## (1) Adjust these variables as needed
@@ -10,10 +10,10 @@
 ## -- on a standard git checkout, this repo it may be ~/git/bh
 pkgdir="${HOME}/git/bh"
 ## -- current boost sources, placed eg in ${pkgdir}/local/
-boosttargz="boost_1_69_0.tar.gz"
+boosttargz="boost_1_72_0.tar.gz"
 ## -- current package version and date (and other metadata as needed)
-version="1.69.0-1"
-date="2018-12-16"
+version="1.72.0-1"
+date="2019-12-15"
 
 
 
@@ -128,10 +128,13 @@ bcp --boost=${boostroot}  ${boostlibs}  ${pkgincl}   > /dev/null  2>&1
 # Plus align (issue ticket #32)
 # Plus sort (issue ticket #35)
 # Plus compute (issue ticket #16 -- and now at last in upstream Boost)
-boostextras="filesystem spirit foreach algorithm iostreams \
+# Minus filesystem (issue ticket #55: cannot used without linking)
+# Plus mp11 (issue ticket #62)
+# Plus polygon (issue ticket #63)
+boostextras="spirit foreach algorithm iostreams \
             dynamic_bitset heap any circular_buffer geometry fusion graph \
             multiprecision phoenix bimap icl flyweight property_tree \
-            scope_exit atomic align sort compute"
+            scope_exit atomic align sort compute mp11 polygon"
 
 echo "Copying (extra) Boost libraries into BH"
 
