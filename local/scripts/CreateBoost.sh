359769c2779ed9af5f0ef952f519c06279daf7c6
Dirk Eddelbuettel <edd@debian.org>
2021-12-12 16:54:28 -0600

    updated helper script for 1.78.0, add beast (closes #81)

--- local/scripts/CreateBoost.sh
+++ local/scripts/CreateBoost.sh
@@ -2,7 +2,7 @@
 ##
 ## CreateBoost.sh -- derived from CreateBoost.R
 ##
-## Jay Emerson and Dirk Eddelbuettel,  2012 - 2020
+## Jay Emerson and Dirk Eddelbuettel,  2012 - 2021
 
 
 ## (1) Adjust these variables as needed
@@ -10,10 +10,10 @@
 ## -- on a standard git checkout, this repo it may be ~/git/bh
 pkgdir="${HOME}/git/bh"
 ## -- current boost sources, placed eg in ${pkgdir}/local/
-boosttargz="boost_1_75_0.tar.gz"
+boosttargz="boost_1_78_0.tar.gz"
 ## -- current package version and date (and other metadata as needed)
-version="1.75.0-0"
-date="2020-12-12"
+version="1.78.0-0"
+date="2021-12-12"
 
 
 
@@ -138,11 +138,12 @@ bcp --boost=${boostroot}  ${boostlibs}  ${pkgincl}   > /dev/null  2>&1
 # Plus polygon (issue ticket #63)
 # Plus accumulators (issue ticket #68)
 # Plus beast (issue ticket #74)
+# Plus lambda2 (issue ticket #81)
 boostextras="spirit foreach algorithm iostreams \
             dynamic_bitset heap any circular_buffer geometry fusion graph \
             multiprecision phoenix bimap icl flyweight property_tree \
             scope_exit atomic align sort compute mp11 polygon accumulators \
-            beast"
+            beast lambda2"
 
 echo "Copying (extra) Boost libraries into BH"
 
